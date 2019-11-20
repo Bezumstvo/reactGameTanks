3d568294fcec6db93495d73495d4af4738eab24b
BeZ <1262708@mail.ru>
2019-11-20 10:13:37 +0300

    restart by Enter

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const socketIO = require('socket.io');
 const app = express();
 const server = http.Server(app);
 const io = socketIO(server);
-
 const params = {
   colors: [
     [false, 'red'],
@@ -20,9 +19,12 @@ const params = {
   ],
   maxPlayers: 4,
   bulletSpeed: 20,
+  bulletTTL: 1000,
+  bounce: true,
   hitArea: 20
 };
 let currNumPlayers = 0;
+let stopGame = false;
 
 app.set('port', 5000);
 app.use('/static', express.static(__dirname + '/static'));
@@ -41,7 +43,7 @@ let bullets = {};
 io.on('connection', function(socket) {
   socket.on('new player', function() {
     if (currNumPlayers < params.maxPlayers) {
-      let color; 
+      let color;
       // find first free color
       let curNumTmp = currNumPlayers;
       while (params.colors[curNumTmp][0]) {
@@ -59,6 +61,7 @@ io.on('connection', function(socket) {
         dy: 0
       };
       currNumPlayers++;
+      socket.emit('meColor', color);
     }
     socket.on('disconnect', function() {
       // test Viewer / Player
@@ -74,74 +77,140 @@ io.on('connection', function(socket) {
 
   socket.on('movement', function(data) {
     let player = players[socket.id] || {};
-    if ( (data.left === 0) && (data.right === 0) ) {
+    if (data.left === 0 && data.right === 0) {
       player.dx = 0;
-    }else{
-      if (data.left>0) {
+    } else {
+      if (data.left > 0) {
         player.dx = -1;
       }
-      if (data.right>0) {
+      if (data.right > 0) {
         player.dx = 1;
       }
     }
-    if ( (data.up === 0) && (data.down === 0) ) {
+    if (data.up === 0 && data.down === 0) {
       player.dy = 0;
-    }else{
-      if (data.up>0) {
+    } else {
+      if (data.up > 0) {
         player.dy = -1;
       }
-      if (data.down>0) {
+      if (data.down > 0) {
         player.dy = 1;
       }
     }
 
     if (data.fire) {
-      let deltaTmp = (data.mouseY-player.y===0) ? 0 : Math.abs( (data.mouseX-player.x)/(data.mouseY-player.y) )**2;
-      deltaX_sign = ( (data.mouseX-player.x)===0 )? 0 : Math.abs(data.mouseX-player.x)/(data.mouseX-player.x);
-      deltaY_sign = ( (data.mouseY-player.y)===0 )? 0 : Math.abs(data.mouseY-player.y)/(data.mouseY-player.y);
-      deltaY_value = Math.sqrt( params.bulletSpeed**2 / (deltaTmp + 1) );
-      deltaX_value = Math.sqrt( params.bulletSpeed**2 - deltaY_value**2);
-      bullets[player.number] = {
-        x: player.x,
-        y: player.y,
-        color: player.color,
-        deltaX: deltaX_sign * deltaX_value,
-        deltaY: deltaY_sign * deltaY_value,
-        number: currNumPlayers
+      if (!bullets[player.number]) {
+        let deltaTmp =
+          data.mouseY - player.y === 0
+            ? 0
+            : Math.abs((data.mouseX - player.x) / (data.mouseY - player.y)) ** 2;
+        deltaX_sign =
+          data.mouseX - player.x === 0
+            ? 0
+            : Math.abs(data.mouseX - player.x) / (data.mouseX - player.x);
+        deltaY_sign =
+          data.mouseY - player.y === 0
+            ? 0
+            : Math.abs(data.mouseY - player.y) / (data.mouseY - player.y);
+        deltaY_value = Math.sqrt(params.bulletSpeed ** 2 / (deltaTmp + 1));
+        deltaX_value = Math.sqrt(params.bulletSpeed ** 2 - deltaY_value ** 2);
+        bullets[player.number] = {
+          x: player.x,
+          y: player.y,
+          color: player.color,
+          deltaX: deltaX_sign * deltaX_value,
+          deltaY: deltaY_sign * deltaY_value,
+          number: currNumPlayers,
+          ttl: new Date().getTime() + params.bulletTTL
+        };
       }
     }
-  }
-);
+  });
+  socket.on('gameOver', data => {
+    stopGame = data;
+    io.sockets.emit('newGame', stopGame);
+  });
+  socket.on('msg', function(data) {
+    let player = players[socket.id] || {};
+    const now = new Date();
+    const hour = now.getHours() < 10 ? '0' + now.getHours() : now.getHours();
+    const min = now.getMinutes() < 10 ? '0' + now.getMinutes() : now.getMinutes();
+    const sec = now.getSeconds() < 10 ? '0' + now.getSeconds() : now.getSeconds();
+    let message = {
+      dateTime: hour + ':' + min + ':' + sec,
+      msg: data,
+      player: player
+    };
+    io.sockets.emit('message', message);
+  });
 });
 
 setInterval(function() {
   for (player in players) {
-    let dx = (players[player].dx===0)? 0 : players[player].dx / Math.abs( players[player].dx)
-    let dy = (players[player].dy===0)? 0 : players[player].dy / Math.abs( players[player].dy)
-    players[player].x += dx;
-    players[player].y += dy;
-    if (players[player].y<0) {players[player].y = 0}
-    if (players[player].x<0) {players[player].x = 0}
-    if (players[player].y>800) {players[player].y = 800}
-    if (players[player].x>1200) {players[player].x = 1200}
+    if (!stopGame) {
+      let dx = players[player].dx === 0 ? 0 : players[player].dx / Math.abs(players[player].dx);
+      let dy = players[player].dy === 0 ? 0 : players[player].dy / Math.abs(players[player].dy);
+      players[player].x += dx;
+      players[player].y += dy;
+    }
+    if (players[player].y < 0) {
+      players[player].y = 0;
+    }
+    if (players[player].x < 0) {
+      players[player].x = 0;
+    }
+    if (players[player].y > 590) {
+      players[player].y = 590;
+    }
+    if (players[player].x > 990) {
+      players[player].x = 990;
+    }
   }
   for (bullet in bullets) {
-    bullets[bullet].x += Math.round( bullets[bullet].deltaX);
-    bullets[bullet].y += Math.round( bullets[bullet].deltaY);
-    for (player in players) {
-      if (bullets[bullet].color!=players[player].color && 
-        bullets[bullet].x > (players[player].x-params.hitArea) &&
-        bullets[bullet].x < (players[player].x+params.hitArea) &&
-        bullets[bullet].y > (players[player].y-params.hitArea) &&
-        bullets[bullet].y < (players[player].y+params.hitArea) ) {
-        io.sockets.emit('hit', players[player]); 
+    if (!stopGame && new Date().getTime() > bullets[bullet].ttl) {
+      delete bullets[bullet];
+    } else {
+      bullets[bullet].x += Math.round(bullets[bullet].deltaX);
+      bullets[bullet].y += Math.round(bullets[bullet].deltaY);
+      for (player in players) {
+        if (
+          bullets[bullet].color != players[player].color &&
+          bullets[bullet].x > players[player].x - params.hitArea &&
+          bullets[bullet].x < players[player].x + params.hitArea &&
+          bullets[bullet].y > players[player].y - params.hitArea &&
+          bullets[bullet].y < players[player].y + params.hitArea
+        ) {
+          stopGame = true;
+          io.sockets.emit('hit', players[player]);
         }
+      }
+      if (params.bounce) {
+        if (bullets[bullet].x > 990) {
+          bullets[bullet].deltaX = -bullets[bullet].deltaX;
+        }
+        if (bullets[bullet].x < 0) {
+          bullets[bullet].deltaX = -bullets[bullet].deltaX;
+        }
+        if (bullets[bullet].y > 590) {
+          bullets[bullet].deltaY = -bullets[bullet].deltaY;
+        }
+        if (bullets[bullet].y < 0) {
+          bullets[bullet].deltaY = -bullets[bullet].deltaY;
+        }
+      } else {
+        if (
+          bullets[bullet].x > 990 ||
+          bullets[bullet].x < 0 ||
+          bullets[bullet].y > 590 ||
+          bullets[bullet].y < 0
+        ) {
+          delete bullets[bullet];
+        }
+      }
     }
-    if (bullets[bullet].x > 1200 || bullets[bullet].x < 0 || bullets[bullet].y > 800 || bullets[bullet].y < 0) {delete bullets[bullet]; }
-  }  
+  }
 }, 1000 / 60);
 
-
-  setInterval(function() {
+setInterval(function() {
   io.sockets.emit('state', players, bullets);
 }, 1000 / 60);
--- static/game.js
+++ static/game.js
@@ -1,5 +1,8 @@
 const socket = io();
-let mouseX = mouseY = 0;
+let mouseX = (mouseY = 0);
+let gameOver = false;
+let gameOverText = 'GAME OVER';
+let meColor;
 const movement = {
   fire: false,
   up: 0,
@@ -10,48 +13,69 @@ const movement = {
   mouseY: false
 };
 document.addEventListener('keydown', function(event) {
-  switch (event.keyCode) {
-    case 65: // A
-      movement.left = true;
-      break;
-    case 87: // W
-      movement.up = true;
-      break;
-    case 68: // D
-      movement.right = true;
-      break;
-    case 83: // S
-      movement.down = true;
-      break;
-    case 70: // F
-     movement.fire = true;
-     movement.mouseX = mouseX;
-     movement.mouseY = mouseY;
-      break;
+  if (document.activeElement.tagName === 'BODY') {
+    switch (event.keyCode) {
+      case 65: // A
+        movement.left = true;
+        break;
+      case 87: // W
+        movement.up = true;
+        break;
+      case 68: // D
+        movement.right = true;
+        break;
+      case 83: // S
+        movement.down = true;
+        break;
+      case 70: // F
+        movement.fire = true;
+        movement.mouseX = mouseX;
+        movement.mouseY = mouseY;
+        break;
+    }
   }
 });
 document.addEventListener('keyup', function(event) {
-  switch (event.keyCode) {
-    case 65: // A
-      movement.left = 0;
-      break;
-    case 87: // W
-      movement.up = 0;
-      break;
-    case 68: // D
-      movement.right = 0;
-      break;
-    case 83: // S
-      movement.down = 0;
-      break;
-    case 70: // F
-      movement.fire = false;
-      movement.mouseX = false;
-      movement.mouseY = false;
+  if (document.activeElement.tagName === 'BODY') {
+    switch (event.keyCode) {
+      case 65: // A
+        movement.left = 0;
+        break;
+      case 87: // W
+        movement.up = 0;
+        break;
+      case 68: // D
+        movement.right = 0;
+        break;
+      case 83: // S
+        movement.down = 0;
+        break;
+      case 70: // F
+        movement.fire = false;
+        movement.mouseX = false;
+        movement.mouseY = false;
+    }
   }
 });
 
+document.querySelector('#input > input').addEventListener('keyup', function(e) {
+  if (e.keyCode === 13) {
+    socket.emit('gameOver', false);
+    const msg = document.querySelector('#input > input').value;
+    document.querySelector('#input > input').value = '';
+    socket.emit('msg', msg);
+  }
+});
+document.querySelector('#input button').addEventListener('click', function(event) {
+  const msg = document.querySelector('#input > input').value;
+  document.querySelector('#input > input').value = '';
+  socket.emit('msg', msg);
+});
+
 socket.emit('new player');
+socket.on('meColor', data => {
+  meColor = data;
+});
 setInterval(function() {
   socket.emit('movement', movement);
 }, 1000 / 60);
@@ -59,40 +83,57 @@ setInterval(function() {
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 
-socket.on('state', function(players,bullets) {
+socket.on('state', function(players, bullets) {
   canvas.width = canvas.clientWidth;
   canvas.height = canvas.clientHeight;
-  context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
-  for (let id in players) {
-    let player = players[id];
-    context.beginPath();
-    context.fillStyle = player.color;
-    context.arc(player.x, player.y, 5, 0, 2 * Math.PI);
-    context.fill();
-  }
-  for (let id in bullets) {
-    let bullet = bullets[id];
-    context.beginPath();
-    context.fillStyle = bullet.color;
-    context.arc(bullet.x, bullet.y, 2, 0, 2 * Math.PI);
-    context.fill();
+  if (!gameOver) {
+    context.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+    for (let id in players) {
+      let player = players[id];
+      context.beginPath();
+      context.fillStyle = player.color;
+      context.arc(player.x, player.y, 5, 0, 2 * Math.PI);
+      context.fill();
+    }
+    for (let id in bullets) {
+      let bullet = bullets[id];
+      context.beginPath();
+      context.fillStyle = bullet.color;
+      context.arc(bullet.x, bullet.y, 2, 0, 2 * Math.PI);
+      context.fill();
+    }
+  } else {
+    context.font = '22px Verdana';
+    context.fillText(gameOverText, 300, 150);
   }
 });
 
 socket.on('hit', function(player) {
-  alert('hit to ', player.color);
-  console.log('hit to ', player.color);
+  console.log('hit to' + player.color);
+  gameOver = true;
+  gameOverText = meColor === player.color ? 'YOU LOSE' : 'YOU WIN';
 });
+socket.on('newGame', function(data) {
+  gameOver = data;
+});
+
+socket.on('message', function(message) {
+  let div = document.createElement('div');
+  div.classList.add('message');
+  div.textContent = message.dateTime + ' ' + message.player.color + ': ' + message.msg;
+  document.querySelector('#history').insertBefore(div, document.querySelector('#history>div'));
+});
+
 function mousemove(event) {
   if (document.attachEvent != null) {
     mouseX = window.event.clientX;
     mouseY = window.event.clientY;
   } else if (!document.attachEvent && document.addEventListener) {
-      mouseX = event.clientX;
-      mouseY = event.clientY;
+    mouseX = event.clientX;
+    mouseY = event.clientY;
   }
-};
+}
 (function initMouseGet() {
   if (document.layers) document.captureEvents(Event.MOUSEMOVE);
   document.onmousemove = mousemove;
-})()
+})();
